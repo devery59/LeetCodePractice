af11b52762c4ca96c58157b39337632d95728824
Kayin <86634292+devery59@users.noreply.github.com>
2025-03-08 11:55:27 +0900

    Time: 2 ms, Memory: 28.3 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,14 +1,16 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s):
         """
-        Do not return anything, modify s in-place instead.
+        :type s: List[str]
+        :rtype: void Do not return anything, modify s in-place instead.
         """
-        i = 0
-        j = len(s)-1
-        while i<j:
-            temp = s[i]
-            s[i] = s[j]
-            s[j] = temp
-            i +=1
-            j -=1
+        def helper(start, end, ls):
+            if start >= end:
+                return
         
+            # swap the first and last element
+            ls[start], ls[end] = ls[end], ls[start]        
+
+            return helper(start+1, end-1, ls)
+    
+        helper(0, len(s)-1, s)
