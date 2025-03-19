7e6c439bd63bddd79c8a305c38ff29d71631d1f7
Kris <86634292+devery59@users.noreply.github.com>
2025-03-19 11:02:32 +0900

    Time: 0 ms (100%), Space: 17.7 MB (78.47%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = [0] * (n + 1)
+        return self.climb_stairs(0, n, memo)
+
+    def climb_stairs(self, i: int, n: int, memo: List[int]) -> int:
+        if i > n:
+            return 0
+        if i == n:
+            return 1
+        if memo[i] > 0:
+            return memo[i]
+        memo[i] = self.climb_stairs(i + 1, n, memo) + self.climb_stairs(i + 2, n, memo)
+        return memo[i]
